31bc92707d25bb40c902d68572cb0a2459481494
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-22 02:16:46 +0600

    object of object

--- js/index.js
+++ js/index.js
@@ -8,6 +8,10 @@ const shop = {
     address:'nurjahan avenue',
     profit: 1500,
     products:['laptop','mobile','pc'],
+    owner:{
+        name:'alia batt',
+        profession:'actor',
+    },
     isExpensive:true,
 }
 const shopSignified = JSON.stringify(shop);
