1d018f4054618bff5bb582630a0e26c7a58fca85
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-22 02:08:43 +0600

    json commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 const user = {id:11,name:'mokbul',job:'software developer',address:'italy'};
-console.log(user);
+const stringified = JSON.stringify(user);
+// console.log(stringified);
+// console.log(user);
+
+const shop = {
+    name:'alias dokan',
+    address:'nurjahan avenue',
+    profit: 1500,
+    products:['laptop','mobile','pc'],
+    isExpensive:true,
+}
+const shopSignified = JSON.stringify(shop);
+console.log(shopSignified);
+console.log(shop);
