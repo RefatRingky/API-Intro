0e41a79d1ca9e9b8c9b5ff0841cffa2e2690c2a8
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-22 14:45:54 +0600

    display user commit

--- api.js
+++ api.js
@@ -3,8 +3,22 @@ function loadData() {
   .then(response => response.json())
   .then(json => console.log(json))
 }
+// json user
 function loadUser() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data =>  displayUser(data) )
+}
+
+// json post
+function loadPost() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+
+function displayUser(data) {
+  for(const user of data){
+    console.log(user.name);
+  }
 }
